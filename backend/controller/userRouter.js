5e7cb8b9330b84d19d75746f6cbcf0b19ab43078
Sahithi-630 <bhumireddysahithi@gmail.com>
2025-03-11 14:55:11 +0530

    test

--- backend/controller/userRouter.js
+++ backend/controller/userRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const uploadUserImage=require("../middlewares/multer");
 const {userModel} = require("../models/userModel");
-
+const bcrypt = require("bcryptjs");
 
 userRouter.post("/signup",uploadUserImage.single("image"),async(req,res)=>{
     try{
@@ -15,7 +15,12 @@ userRouter.post("/signup",uploadUserImage.single("image"),async(req,res)=>{
             return res.status(200).send({message:"User Already Exists"});
         }
 
-        const newUser = await userModel.insertOne({name,email,password});
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+
+
+        const newUser = await userModel.insertOne({name,email,password:hash});
+
         return res.status(200).send({message:"User registered successfully"})
 
     }catch (error) {
@@ -29,14 +34,18 @@ userModel.post("/login",async(req,res)=>{
         if( email!=""|| password!=""){
             return res.status(400).send({message:"All fields are required"});
         }
-        const user =await userModel.findOne({email:email,password:password});
-        if(user){
-            return res.status(200).send({message:"User Already Exists"});
+
+        
+        const user =await userModel.findOne({email});
+        const matchedPass = bcrypt.compareSync(password, hash);
+        if(user && matchedPass){
+            return res.status(200).send({message:"User loged in successfully"});
         }
-        return res.status(500).send({message:"User loged in successfully"})
+        
+        return res.status(401).send({message:"Entered details are wrong"});
 
     }catch(error){
-        return res.status(500).send({message:"Something went wrong"});
+        return res.status(500).send({message:"Error"});
     }
     
 })
