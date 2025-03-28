acb39819cb0e58e06ec3ba0e22510cde95588bfe
Sahithi-630 <bhumireddysahithi@gmail.com>
2025-03-28 15:19:06 +0530

    test

--- backend/controller/allProducts.js
+++ backend/controller/allProducts.js
@@ -1,26 +1,27 @@
 const express = require("express");
 const allProductRouter = express.Router();
 const productModel = require("../models/productModel");
-allProductRouter.get("/",async(req,res)=>{
+
+allProductRouter.get("/", async (req, res) => {
     try {
         const products = await productModel.find();
-        return res.status(200).send({message:"Sucessfull",products:products})
+        return res.status(200).send({ message: "Successful", products: products });
     } catch (error) {
-        return res.status(500).send({message:"Something went wrong"});
+        return res.status(500).send({ message: "Something went wrong" });
     }
-})
+});
 
-allProductRouter.get("/:id",async(req,res)=>{
+allProductRouter.get("/:id", async (req, res) => {
     try {
-        const {id} = req.params;
-        if(!id){
-            return req.status(400).send({message:"Please provide id"});
+        const { id } = req.params;
+        if (!id) {
+            return res.status(400).send({ message: "Please provide id" }); // Corrected this line
         }
-        const product = await productModel.findOne({_id:id});
-        return res.status(200).send({message:"Sucessfull",product:product})
+        const product = await productModel.findOne({ _id: id });
+        return res.status(200).send({ message: "Successful", product: product });
     } catch (error) {
-        return res.status(500).send({message:"Something went wrong"});
+        return res.status(500).send({ message: "Something went wrong" });
     }
-})
+});
 
-module.export = allProductRouter;
+module.exports = allProductRouter; 
